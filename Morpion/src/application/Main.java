9aae3b6ea3df4065b8431f62bea8b122ed223ca3
romain-rigaud <117656441+romain-rigaud@users.noreply.github.com>
2023-03-03 08:56:16 +0100

    Add files via upload

---
+++ Morpion/src/application/Main.java
@@ -0,0 +1,49 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("Morpion");
+			
+			// Load root layout from fxml file
+			FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("Sample.fxml"));
+            BorderPane Sample = (BorderPane) loader.load();
+			Scene scene = new Scene(Sample,800,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Show the scene containing the root layout.
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			Button bouton = new Button();
+			test(scene);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void test(Scene scene) {
+		Button bouton = (Button) scene.lookup("#bouton lancer");
+		bouton.setOnMouseClicked(event ->{
+			bouton.setText("salut");
+		});
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+}
