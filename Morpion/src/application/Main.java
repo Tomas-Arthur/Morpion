5f114ae03723a209af664c26bf1cb34814116b01
Tomas-arthur <tomas.arthur20@gmail.com>
2023-03-08 22:00:47 +0100

    boutton setConfig ouvre une nouvelle fenetre avec les config dans un textarea

--- Morpion/src/application/Main.java
+++ Morpion/src/application/Main.java
@@ -1,4 +1,10 @@
 package application;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import ai.Test;
 
 import javafx.application.Application;
@@ -7,25 +13,34 @@ import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application implements Runnable{
 
 	static Scene scene;
+	static Stage primaryStage;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.setTitle("Morpion");
-			
+			this.primaryStage = primaryStage;
 			// Load root layout from fxml file
 			FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("Sample.fxml"));
@@ -75,32 +90,21 @@ public class Main extends Application implements Runnable{
 						lancerDuLearn.setDisable(false);
 					}
 				});
-	             /*for (iterations = 0; iterations < 10000000; iterations++) {
-	            	 System.out.println("Iteration " + iterations);
-	                 if (isCancelled()) {
-	                     updateMessage("Cancelled");
-	                     break;
-	                 }
-	                 //notify();
-	                 updateMessage("Iteration " + iterations);
-	                 
-	                 updateProgress(iterations, 10000);
-	             }*/
+	            
 				return null;
 	         }
 	     };
-	     /*
-	     task.progressProperty().addListener((observable, oldValue, newValue) -> {
-	    	    System.out.println("progressProperty changed from " + oldValue + " to " + newValue);
-	    	    avancementDuLearn.setText(newValue.toString());
-	     });
-	     */
+	     
+	     
+	     
 	     
 			Thread t = new Thread(task);
 			t.setDaemon(true);
 			t.start();
 			lancerDuLearn.setDisable(true);
 		});
+		
+		
 		
 	}
 	
@@ -121,6 +125,72 @@ public class Main extends Application implements Runnable{
 		
 	}
 
+	public void setConfig()
+	{
+		
+        Label secondLabel = new Label("I'm a Label on new Window");
+
+        StackPane secondaryLayout = new StackPane();
+        secondaryLayout.getChildren().add(secondLabel);
+        VBox config = new VBox();
+        TextArea confAffichage = new TextArea();
+        secondaryLayout.getChildren().addAll(config,confAffichage);
+        Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+		// File path is passed as parameter
+        File file = new File(
+            "./resources/config.txt");
+ 
+        // Note:  Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+ 
+        // Creating an object of BufferedReader class
+        BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 
+        // Declaring a string variable
+        String st;
+        String conf = "";
+        // Condition holds true till
+        // there is character in a string
+        try {
+			while ((st = br.readLine()) != null)
+			{
+			    // Print the string
+			    //System.out.println(st);
+			    conf+=st+'\n';
+				confAffichage.setText(conf);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+
+        // New window (Stage)
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Second Stage");
+        newWindow.setScene(secondScene);
+
+        // Specifies the modality for new window.
+        newWindow.initModality(Modality.WINDOW_MODAL);
+
+        // Specifies the owner Window (parent) for new window
+        newWindow.initOwner(primaryStage);
+
+        // Set position of second window, related to primary window.
+        newWindow.setX(primaryStage.getX() + 200);
+        newWindow.setY(primaryStage.getY() + 100);
+
+        newWindow.show();
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
